0970f1d13e83084d7ee002b15c2f6e822ff23df8
biaiamauchi <biaiamauchi@hotmail.com>
2021-05-19 02:33:03 -0300

    Construtor e montador do tabuleiro finalizados

--- mc322/src/mc322/lab06/bibliotecas/Controle.java
+++ mc322/src/mc322/lab06/bibliotecas/Controle.java
@@ -11,13 +11,248 @@ public class Controle {
     public Controle(){
         //construtor
     }
-
     //Bia - 1 a 3 + montador
-    //1 - recebe da main o CSV de entrada
+    public void ConstruirTabueiro(String caminhoCSV){
+        CSVHandling posicoes = new CSVHandling();
+        posicoes.setDataSource(caminhoCSV);
+        String matrizComandos[][] = posicoes.requestCommands();
+        //[[1:1, P], [1:2, _], ...,[4:4, _]];
+        if (verificarTabuleiro(matrizComandos)){
+            montador.montaTabuleiro(matrizComandos);
+        }
+    }
 
-    //2 - verificar se a entrada dá pra fazer um jogo (de 2 a 3 buracos, 1 monstro, 1 ouro, 1 herói em (1,1))
+    public boolean verificarTabuleiro(String[][] matrizComandos){
+        int n_buracos = 0;
+        int n_wumpus = 0;
+        int n_heroi = 0;
+        int n_ouro = 0;
+        if (!matrizComandos[0][1].equals("P")){
+            return false;
+        }
+        for (int i = 0; i < 16; i++){
+            if (matrizComandos[i][1].equals("P")) {
+                n_heroi++;
+            } else if (matrizComandos[i][1].equals("W")) {
+                n_wumpus++;
+            } else if (matrizComandos[i][1].equals("B")){
+                n_buracos++;
+            } else if (matrizComandos[i][1].equals("O")){
+                n_ouro++;
+            }
+        }
+        if ((n_buracos == 3 || n_buracos == 2) && n_heroi == 1 && n_ouro == 1 && n_wumpus == 1){
+            return true;
+        }
+        return false;
+    }
 
-    //3 - chama o montador para começar as construções das salas e caverna
+    //correto:
+        /*
+        1:1,P
+        1:2,_
+        1:3,B
+        1:4,_
+        2:1,_
+        2:2,_
+        2:3,O
+        2:4,_
+        3:1,W
+        3:2,_
+        3:3,_
+        3:4,_
+        4:1,_
+        4:2,_
+        4:3,B
+        4:4,_
+        */
+        /*
+        1:1,P
+        1:2,_
+        1:3,B
+        1:4,_
+        2:1,_
+        2:2,_
+        2:3,O
+        2:4,_
+        3:1,W
+        3:2,_
+        3:3,_
+        3:4,B
+        4:1,_
+        4:2,_
+        4:3,B
+        4:4,_
+        */
+    //n buracos errado:
+        /*
+        1:1,P
+        1:2,_
+        1:3,B
+        1:4,_
+        2:1,B
+        2:2,_
+        2:3,O
+        2:4,_
+        3:1,W
+        3:2,_
+        3:3,_
+        3:4,B
+        4:1,_
+        4:2,_
+        4:3,B
+        4:4,_
+        */
+        /*
+        1:1,P
+        1:2,_
+        1:3,_
+        1:4,_
+        2:1,_
+        2:2,_
+        2:3,O
+        2:4,_
+        3:1,W
+        3:2,_
+        3:3,_
+        3:4,_
+        4:1,_
+        4:2,_
+        4:3,B
+        4:4,_
+        */
+    //n herois errado:
+        /*
+        1:1,P
+        1:2,_
+        1:3,B
+        1:4,_
+        2:1,P
+        2:2,_
+        2:3,O
+        2:4,_
+        3:1,W
+        3:2,_
+        3:3,_
+        3:4,_
+        4:1,_
+        4:2,_
+        4:3,B
+        4:4,_
+        */
+        /*
+        1:1,_
+        1:2,_
+        1:3,B
+        1:4,_
+        2:1,_
+        2:2,_
+        2:3,O
+        2:4,_
+        3:1,W
+        3:2,_
+        3:3,_
+        3:4,_
+        4:1,_
+        4:2,_
+        4:3,B
+        4:4,_
+        */
+    //heroi fora do 11:
+        /*
+        1:1,_
+        1:2,_
+        1:3,B
+        1:4,_
+        2:1,_
+        2:2,_
+        2:3,O
+        2:4,_
+        3:1,W
+        3:2,_
+        3:3,_
+        3:4,P
+        4:1,_
+        4:2,_
+        4:3,B
+        4:4,_
+        */
+    //n monstro errado:
+        /*
+        1:1,P
+        1:2,_
+        1:3,B
+        1:4,_
+        2:1,_
+        2:2,_
+        2:3,O
+        2:4,_
+        3:1,_
+        3:2,_
+        3:3,_
+        3:4,_
+        4:1,_
+        4:2,_
+        4:3,B
+        4:4,_
+        */
+        /*
+        1:1,P
+        1:2,_
+        1:3,B
+        1:4,_
+        2:1,_
+        2:2,_
+        2:3,O
+        2:4,_
+        3:1,W
+        3:2,_
+        3:3,_
+        3:4,W
+        4:1,_
+        4:2,_
+        4:3,B
+        4:4,_
+        */
+    //n ouro errado:
+        /*
+        1:1,P
+        1:2,_
+        1:3,B
+        1:4,_
+        2:1,_
+        2:2,_
+        2:3,_
+        2:4,_
+        3:1,W
+        3:2,_
+        3:3,_
+        3:4,_
+        4:1,_
+        4:2,_
+        4:3,B
+        4:4,_
+        */
+        /*
+        1:1,P
+        1:2,_
+        1:3,B
+        1:4,_
+        2:1,_
+        2:2,_
+        2:3,O
+        2:4,_
+        3:1,W
+        3:2,_
+        3:3,_
+        3:4,O
+        4:1,_
+        4:2,_
+        4:3,B
+        4:4,_
+        */
+    //cai no buraco
+    //enfrenta monstro
 
     //Pedro - 4 a 6
     public void movimentar(){
--- mc322/src/mc322/lab06/bibliotecas/Montador.java
+++ mc322/src/mc322/lab06/bibliotecas/Montador.java
@@ -1,5 +1,66 @@
 package mc322.lab06.bibliotecas;
 
+import mc322.lab06.componentes.Heroi;
+import mc322.lab06.componentes.Buraco;
+import mc322.lab06.componentes.Ouro;
+import mc322.lab06.componentes.Wumpus;
+import mc322.lab06.componentes.Fedor;
+import mc322.lab06.componentes.Brisa;
+
 public class Montador {
     //instancia os componentes contidos no arquivo de leitura
+    private Caverna caverna;
+    public void montaTabuleiro(String[][] matrizComandos){
+        for (int i = 0; i < 16; i++){
+            int linha = i / 4;
+            int coluna = i % 4;
+            if (matrizComandos[i][1].equals("W")) {
+                caverna.setCelulaCaverna(new Wumpus("W", linha, coluna), linha, coluna);
+            } else if (matrizComandos[i][1].equals("B")){
+                caverna.setCelulaCaverna(new Buraco("W", linha, coluna), linha, coluna);
+            } else if (matrizComandos[i][1].equals("O")){
+                caverna.setCelulaCaverna(new Ouro("W", linha, coluna), linha, coluna);
+            } else if (matrizComandos[i][1].equals("P")) {
+                caverna.setCelulaCaverna(new Heroi("P", linha, coluna), linha, coluna);
+            } else if (matrizComandos[i][1].equals("_")){
+                caverna.setCelulaCaverna(null, linha, coluna);
+            }
+        }
+        verificaBrisaFedor(caverna);
+    }
+
+    public void verificaBrisaFedor(Caverna caverna){
+        // chamar sempre que o monstro for derrotado ou que o ouro for coletado
+        for (int linha = 0; linha < 4; linha++){
+            for (int coluna = 0; coluna < 4; coluna++){
+                if (caverna.getCelulaCaverna(linha, coluna).getPreenchimento() == "W") {
+                    if (caverna.getCelulaCaverna(linha + 1, coluna).getPreenchimento() == null) {
+                        caverna.setCelulaCaverna(new Fedor("f", linha + 1, coluna), linha + 1, coluna);
+                    }
+                    if (caverna.getCelulaCaverna(linha, coluna + 1).getPreenchimento() == null) {
+                        caverna.setCelulaCaverna(new Fedor("f", linha, coluna + 1), linha, coluna + 1);
+                    }
+                    if (caverna.getCelulaCaverna(linha - 1, coluna).getPreenchimento() == null) {
+                        caverna.setCelulaCaverna(new Fedor("f", linha - 1, coluna), linha - 1, coluna);
+                    }
+                    if (caverna.getCelulaCaverna(linha, coluna - 1).getPreenchimento() == null) {
+                        caverna.setCelulaCaverna(new Fedor("f", linha, coluna - 1), linha, coluna - 1);
+                    }
+                } else if (caverna.getCelulaCaverna(linha, coluna).getPreenchimento() == "B") {
+                    if (caverna.getCelulaCaverna(linha + 1, coluna).getPreenchimento() == null) {
+                        caverna.setCelulaCaverna(new Brisa("b", linha + 1, coluna), linha + 1, coluna);
+                    }
+                    if (caverna.getCelulaCaverna(linha, coluna + 1).getPreenchimento() == null) {
+                        caverna.setCelulaCaverna(new Brisa("b", linha, coluna + 1), linha, coluna + 1);
+                    }
+                    if (caverna.getCelulaCaverna(linha - 1, coluna).getPreenchimento() == null) {
+                        caverna.setCelulaCaverna(new Brisa("b", linha - 1, coluna), linha - 1, coluna);
+                    }
+                    if (caverna.getCelulaCaverna(linha, coluna - 1).getPreenchimento() == null) {
+                        caverna.setCelulaCaverna(new Brisa("b", linha, coluna - 1), linha, coluna - 1);
+                    }
+                }
+            }
+        }
+    }
 }
