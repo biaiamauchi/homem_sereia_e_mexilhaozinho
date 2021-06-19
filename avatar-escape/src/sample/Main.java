dfa9c21bf176219a7e9466b19f3d0173ed585f20
Pedro Salvador <p187044@dac.unicamp.br>
2021-06-18 22:18:28 -0300

    JavaFX
    Entendendo como funciona

--- avatar-escape/src/sample/Main.java
+++ avatar-escape/src/sample/Main.java
@@ -2,23 +2,43 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.awt.*;
+import javafx.scene.shape.Rectangle;
+import java.lang.Object;
+import javafx.scene.control.Label;
 
+import javafx.scene.paint.Color;
+
+
+public class Main extends Application {
     public Parent createContent(){
-        return new StackPane(new Text("Hello Word"));
+        StackPane painelInfo = new StackPane();
+        painelInfo.getChildren().addAll(new Rectangle(300,600,Color.BLUE));
+        painelInfo.setAlignment(Pos.CENTER_RIGHT);
+
+        BackgroundImage fundo = new BackgroundImage(new Image("../../assets/backgrounds/junina.jpeg"), null, null, null, null);
+
+        StackPane tabuleiro = new StackPane();
+        tabuleiro.getChildren().addAll(new Rectangle(900,600,Color.GRAY));
+        tabuleiro.setAlignment(Pos.CENTER_LEFT);
+
+        return new StackPane(painelInfo, tabuleiro);
     }
 
     @Override
     public void start(Stage stage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        stage.setTitle("Hello World");
-        stage.setScene(new Scene(createContent(), 300, 275));
+        stage.setTitle("Avatar Escape");
+        stage.setScene(new Scene(createContent(), 1200, 600));
         stage.show();
     }
 
